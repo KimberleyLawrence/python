14ee8c5422f96a3d0e949f19a5d8f65329b3240a
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-16 23:23:26 -0700

    set up of a tip calculator
    Tax needs to be divided by 100 in order to get a decimal number; in
    this case the tax is 6.75%, this was then / 100 to form a decimal.
    Same for the tip, the tip percentage was 15%, but add the .0 after the
    15 to create a decimal then / 100

---
+++ tipcalculatorsetup.py
@@ -0,0 +1,8 @@
+# You're almost there! Assign the tip variable on line 5.
+
+meal = 44.50
+tax = 0.0675
+# tax was divided by 100, 6.75/100 in order to get the decimal
+tip = 15.0 / 100
+# tip of 15% is divided by 100 in order to get a decimal number
+# add .0 after the number of the percentage of the tip to get a decimal
