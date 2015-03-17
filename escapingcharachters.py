1f638663c03be8639bfad0c13e08f732eb4be06d
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-17 14:23:52 -0700

    Escaping Characters
    When using apostrophes in a sentence, the code will see it as a string
    and the code will break. Using a backslash \ before the apostrophe will
    stop it from breaking.

---
+++ escapingcharachters.py
@@ -0,0 +1,7 @@
+# The string below is broken. Fix it using the escape backslash!
+
+'This isn't flying, this is falling with style!'
+
+#use the backslash before the apostrophe the code doesn't break
+
+'This isn\'t flying, this is falling with style!'
