3d26778b0153215269b0dbfdf5522189549ab362
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-17 22:03:51 -0700

    Dot notation
    Dot notation “.upper()” can only be used on a string. Opposed to the
    “len()” and “str()” as they work well with other data types.

---
+++ dotnotation.py
@@ -0,0 +1,5 @@
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+#dot notation ".upper()" only work with strings. The "len() and str() work with other data types.
