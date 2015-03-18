cf88eb618b08d4c99c48e3fcd8cb700e1496415e
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 15:37:16 -0700

    String review

---
+++ stringreview.py
@@ -0,0 +1,6 @@
+# Write your code below, starting on line 3!
+
+my_string = "wedding"
+print len(my_string)
+print my_string.upper()
+#remember not to put my_string into quotations for the .upper() otherwise that will print.
