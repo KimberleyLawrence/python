356be6f709632a8603840897d64715d2f7b442a3
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 16:42:09 -0700

    Printing date and time on one line
    mm/dd/year hh:mm:ss
    Printed all on one line

---
+++ mm:dd:yyyyhh;mm;ss.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+now = datetime.now()
+
+print '%s/%s/%s' % (now.month, now.day, now.year)
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
+print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
