0dc8eb17dd247400cd1c7234818a52874632b7ca
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-17 22:43:08 -0700

    String Concatenation
    Using the math operators you can combine different strings together to
    print a sentence. Just remember to add a space at the end of each word
    that has a word next to it to make sure it doesn’t just come out like
    one long word.

---
+++ Stringconcatenation.py
@@ -0,0 +1,4 @@
+# Print the concatenation of "Spam and eggs" on line 3!
+
+print "Spam " + "and " + "eggs"
+"""remember to add a space at the end of the words to create space in the sentance otherwise it will come out as one full word "spamandeggs" rather than spam and eggs"""
