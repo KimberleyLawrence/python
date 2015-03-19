fe1881a075546b89211a00cab4170471c1614f19
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 23:36:53 -0700

    Boolean operation combinations
    This time, it was required to create the combination of boolean
    operators to create the expected result

---
+++ booleanoperationcombination.py
@@ -0,0 +1,16 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = True and True
+
+# Make me false!
+bool_three = not True 
+
+# Make me true!
+bool_four = "orange" == "orange"
+
+# Make me true!
+bool_five = "cat" + "lady" == "catlady"
