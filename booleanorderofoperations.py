549ff59921f08ece5c9fcf1d40ee960084c075ee
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 23:21:17 -0700

    Boolean order of operations
    Like in math (bodmas), Boolean operations are completed in a certain
    order.
    1st Not
    2nd And
    3rd Or
    Using () will make sure that your expressions are evaluated in the
    order that you want.

---
+++ booleanorderofoperations.py
@@ -0,0 +1,15 @@
+bool_one = False or not True and True
+
+bool_two = False and not True or True
+
+bool_three = True and not (False or False)
+
+bool_four = not not True or False and not True
+
+bool_five = False or not (True and True)
+
+print bool_one
+print bool_two
+print bool_three
+print bool_four
+print bool_five
