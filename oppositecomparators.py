eb1a4d6f7650232cc3b8d5bd3aa75f14fa6a4a67
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 18:34:09 -0700

    understanding boolean operators
    and : checks if both the statements are true
    or : checks if one of the statements is true
    not: gives the opposite of the statement

---
+++ oppositecomparators.py
@@ -0,0 +1,16 @@
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5  # We already did this one for you!
+
+# Make me false!
+bool_two = 24 >= 35
+
+# Make me true!
+bool_three = 50 + 2 != 35
+
+# Make me false!
+bool_four = 2 + 5 == 4
+
+# Make me true!
+bool_five = 3 - 2 == 1
