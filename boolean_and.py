1fb20ea94913be402c3d18cbdc1b493896747e79
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-18 22:28:40 -0700

    Boolean And
    “And” checks if both of the statements are true.
    Bool_one is false and false compared to bool_five which is true and true

---
+++ boolean_and.py
@@ -0,0 +1,9 @@
+bool_one = 2>4 and 4>8
+
+bool_two = -(-(-(-2))) == -2 and 4 >= 16**0.5
+
+bool_three = 19 % 4!= 300 / 10 / 10 and False
+
+bool_four = -(1**2) < 2**0 and 10 % 10 <= 20 - 10
+
+bool_five = 3<4 and 7<8
