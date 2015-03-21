9969591236904d3279e45c424ce3d1a3f7150758
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-20 23:49:32 -0700

    Review of Conditions and Control Flow

---
+++ conditionalreview.py
@@ -0,0 +1,16 @@
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if 2 <= 2:
+        print "if worked"
+    
+        # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+    elif  not False :
+        print "elif worked"
+        # Keep going here.
+        # You'll want to add the else statement, too!
+    else :
+        print "else worked"
+        
+    return True
