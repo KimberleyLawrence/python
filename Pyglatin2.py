8e7b7e3486453ed2cdaf456cc97a671ac70bd46a
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-21 22:27:01 -0700

    Pyg latin 2
    Adding .lower() to get the word typed in into lower case.
    Then getting the first letter from the word.

---
+++ Pyglatin2.py
@@ -0,0 +1,12 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    print original
+    print word
+    print first
+else:
+    print 'empty'
