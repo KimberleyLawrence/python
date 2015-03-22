24b9ae1e8711823ba3d60bdce59d921ae0d13169
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-21 22:34:58 -0700

    Pyg Latin 3
    Moving the first character to the end of the word, and adding the ay
    into the end of the word.

---
+++ Pyglatin3.py
@@ -0,0 +1,14 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    new_word = word + first + pyg
+    print original
+    print word
+    print first
+    print new_word
+else:
+    print 'empty'
