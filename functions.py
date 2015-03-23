9132b4baf0fd4b8c693b91889f5f5fafeb241533
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-22 22:44:49 -1000

    Functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Define your spam function starting on line 5. You
+# can leave the code on line 11 alone for now--we'll
+# explain it soon!
+
+spam = "Eggs!"
+def spam():
+    """ Prints 'Eggs!' into the console"""
+    print "Eggs!"
+
+
+
+
+# Define the spam function above this line.
+spam()
