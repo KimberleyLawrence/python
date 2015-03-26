be749504be2d46077e9cb36819ce04913845f47d
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-25 17:18:08 -0700

    Parameters and Arguments
    A parameter acts as a variable name for a passed in argument
    NOTE: A function can have as many parameters as you like, but when you
    call the function, you should pass the same amounts of arguments.

--- parameters.py
+++ parameters.py
@@ -3,3 +3,4 @@ def power(base, exponent):  # Add your parameters here!
     print "%d to the power of %d is %d." % (base, exponent, result)
 
 power(37,4)  # Add your arguments here!
+# console prints 37 to the power of 4 is 1874161
