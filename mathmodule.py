39b940a037e15aa3aac1dce964057ae303f702f0
KimberleyLawrence <kimberleynedic@gmail.com>
2015-03-26 16:43:54 -0700

    Math module and dangers of Universal imports
    Universal imports look great on the surface but can create problems in
    your code.
    They fill your program with a lot of variables and function names
    without any safety of those names being associated with the modules
    they came from.
    e.g. if you have a function of your own named sort, and you import
    math, your function is safe as there is your sort and math.sqrt.
    But if you do from math import * (universal import) you have a problem,
    two different functions with the exact same name.
    Even if your own definitions don’t directly conflict with names from
    the imported modules, if you import* from several modules at once, it
    will be hard to figure out which variable and function came from where.
    The code below prints all the functions from the math module.

---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+import math            # Imports the math module
+everything = dir(math) # Sets everything to a list of things from math
+print everything       # Prints 'em all!
+""" this was an example from Codecadamy, this is the result in the console 
+['__doc__', '__name__', '__package__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil',
+ 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod',
+  'frexp', 'fsum', 'gamma', 'hypot', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'modf', 'pi', 
+  'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc']"""
