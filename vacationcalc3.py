1c449cdc013a6bb1b9c45f69c2ca116f6e26b989
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-05 22:53:25 -0700

    vacation calc - rental car
    remember to add the extra variable “cost” to the mix

---
+++ vacationcalc3.py
@@ -0,0 +1,20 @@
+def hotel_cost(nights):
+    return 140 * nights
+
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+
+def rental_car_cost(days):
+    cost = 40 * days
+    if days >= 7:
+        cost -= 50
+    elif days >= 3:
+        cost -=20
+    return cost
