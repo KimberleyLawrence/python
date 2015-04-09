6223207142be1e893c8aa4f6e0a92428f2c50208
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-08 21:31:45 -0700

    adding arguments with append
    rather than remembering or counting the index, using .append will add
    extra items into the argument.

---
+++ append.py
@@ -0,0 +1,14 @@
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append('hat')
+suitcase.append('wallet')
+suitcase.append('towel')
+
+
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
