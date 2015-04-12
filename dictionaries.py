b2e09d4d341e932cc4cee4d51ad1ec5cbd16ad61
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-12 10:58:54 -0400

    Dictionaries {}
    Can be changed after they are created, just lists  - “mutable” -
    mutation.
    The empty pair of {} is just an empty dictionary.
    len() is the length of the key value pairs it has in the dictionary,
    you can also put lists into dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50
+menu['apple'] = 3.75
+menu['beer'] = 5
+menu['wine'] = 200# Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+
+
+
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
