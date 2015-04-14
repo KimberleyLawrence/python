912fe9ee7220a6cb01981d504bd9a8bdf6b4f02c
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-13 19:39:50 -0700

    Using the for loop to work out if numbers are even.
    see notes in code
    make sure you keep indentation to make it easier to read

---
+++ for_loop_even_numbers.py
@@ -0,0 +1,5 @@
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+for number in a:
+    # % 2 == 0 is dividing the number by 2, and seeing if there is any remainders, any remainders mean that the number is not even.
+    if number % 2 == 0:
+        print number
