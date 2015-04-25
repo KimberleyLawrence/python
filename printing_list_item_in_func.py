671564aa81d74651ea3b38d336e18cbf29036ceb
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-25 12:54:03 -0700

    Printing a list item by item in function
    the for code is to print out each element one by one

---
+++ printing_list_item_in_func.py
@@ -0,0 +1,7 @@
+n = [3, 5, 7]
+#define function print_list with argument x
+def print_list(x):
+    #existing code - replaced i with x in code 
+    for x in range(0, len(n)):
+        #remember to indent again! 
+        print n[x]
