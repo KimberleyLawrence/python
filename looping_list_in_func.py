79d0f2988d3edf6439d41d10dda3942e618eb086
KimberleyLawrence <kimberleynedic@gmail.com>
2015-04-28 18:41:44 -0700

    Looping a list in a funtion
    see notes in code

---
+++ looping_list_in_func.py
@@ -0,0 +1,10 @@
+n = [3, 5, 7]
+
+def total(numbers):
+    result = 0
+    for number in numbers:
+        # adds each number into result. This will continue through the list, index 0 will =  3, then index 1 will = 8, then index 2 will = 15
+        result= result + number 
+    #return result
+    return result
+#return numbers[result]
