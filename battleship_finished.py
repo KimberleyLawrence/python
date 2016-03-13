25aea94003e6ee9ebd9da6cbc60ec6c7e5a62564
KimberleyLawrence <kimberleynedic@gmail.com>
2016-03-12 20:48:42 -0800

    adding a break to end the for loop if the player guessing the right spot

--- battleship_finished.py
+++ battleship_finished.py
@@ -31,6 +31,8 @@ for turn in range(4):
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        break
+
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
